98d9ab41bba0b7477ead2758b177437743afaee6
Murat GOKDUMAN <drmuratgokduman@gmail.com>
2023-05-17 08:31:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import Header from "./components/header/Header";
+import Content from "./components/content/Content";
+import Footer from "./components/Footer/Footer";
+import { useSelector } from "react-redux";
+import Final from "./components/final/Final";
+
+const Container = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  padding: 20px 0;
+`;
 
 function App() {
+  const status = useSelector((state) => state.cards.status);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header />
+        <Content />
+      </Container>
+      <Footer />
+      {status && <Final status={status}/>}
     </div>
   );
 }
---
+++ src/components/header/Header.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 2em;
+  color: palevioletred;
+`;
+
+export const Head = styled.div`
+  bacgroun-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 15px;
+`;
